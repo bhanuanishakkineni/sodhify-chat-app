62716aa294d1e992290b18550030ab9f32ce9c0f
Bhanu Anish <bhanuanishakkineni77@gmail.com>
2026-01-27 20:35:36 -0600

    add script to run Frontend and Backend together

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,12 +1,25 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import appRouter from './route.js';
+import path from 'path';
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
 appRouter(app);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../Frontend/dist')));
+    app.use((req, res, next) => {
+        // Skip API routes
+        if (req.path.startsWith('/api')) {
+            return next();
+        }
+        res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"));
+    });
+}
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
